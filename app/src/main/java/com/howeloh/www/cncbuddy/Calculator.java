1a84c092bd85891a44d97d9f07fdaecf9c035614
Eric Lohmar <ghandi263@howloh.com>
2015-11-02 06:01:56 -0600

    Reconfigured Focus Listeners and added to remaining fields. Completed Equations and related methods in Calculator class. Began the Milling Activity.

--- app/src/main/java/com/howeloh/www/cncbuddy/Calculator.java
+++ app/src/main/java/com/howeloh/www/cncbuddy/Calculator.java
@@ -8,37 +8,232 @@ import android.widget.EditText;
  */
 public class Calculator {
 
-    EditText Dia;
-    EditText SFM;
-    EditText RPM;
-    EditText IPR;
-    EditText IPM;
-
-    private String calculateDia_w_SFM_RPM(EditText SFM, EditText RPM){
+    private String calculateDia_w_SFM_RPM(EditText SFM, EditText RPM) {
         return String.format("%.4f", Double.valueOf(SFM.getText().toString()) / (Double.valueOf(RPM.getText().toString()) * (Math.PI / 12d)));
     }
 
-    private String calculateSFM_w_Dia_RPM (EditText Dia, EditText RPM){
+    private String calculateSFM_w_Dia_RPM(EditText Dia, EditText RPM) {
         return String.format("%.4f", Double.valueOf(Dia.getText().toString()) * (Double.valueOf(RPM.getText().toString()) * (Math.PI / 12d)));
     }
 
-    private String calculateRPM_w_Dia_SFM(EditText Dia, EditText SFM){
-        return String.format("%.4f", Double.valueOf(SFM.getText().toString()) /(Double.valueOf(Dia.getText().toString()) * (Math.PI / 12d)));
+    private String calculateRPM_w_Dia_SFM(EditText Dia, EditText SFM) {
+        return String.format("%.4f", Double.valueOf(SFM.getText().toString()) / (Double.valueOf(Dia.getText().toString()) * (Math.PI / 12d)));
     }
 
-    private String calculateRPM_w_IPR_IPM (EditText IPR, EditText IPM){
+    private String calculateRPM_w_IPR_IPM(EditText IPR, EditText IPM) {
         return String.format("%.4f", Double.valueOf(IPM.getText().toString()) / Double.valueOf(IPR.getText().toString()));
     }
 
-    private String calculateIPM_w_RPM_IPR (EditText RPM, EditText IPR){
+    /*private String calculateRPM_w_IPR_IPM_Cut(EditText IPR, EditText IPM, EditText Cut) {
+        return String.format("%.4f", (Double.valueOf(IPM.getText().toString()) / Double.valueOf(Cut.getText().toString())) / Double.valueOf(IPR.getText().toString()));
+    }*/
+
+    private String calculateIPM_w_RPM_IPR(EditText RPM, EditText IPR) {
         return String.format("%.4f", Double.valueOf(RPM.getText().toString()) * Double.valueOf(IPR.getText().toString()));
     }
 
-    private String calculateIPR_w_RPM_IPM (EditText RPM, EditText IPM){
+    /*private String calculateIPM_w_RPM_IPR_Cut(EditText RPM, EditText IPR, EditText Cut) {
+        return String.format("%.4f", Double.valueOf(RPM.getText().toString()) * Double.valueOf(IPR.getText().toString()) * Double.valueOf(Cut.getText().toString()));
+    }*/
+
+    private String calculateIPR_w_RPM_IPM(EditText RPM, EditText IPM) {
         return String.format("%.4f", Double.valueOf(IPM.getText().toString()) / Double.valueOf(RPM.getText().toString()));
     }
 
-    private void diaLostFocus (){
+    /*private String calculateIPR_w_RPM_IPM_Cut(EditText RPM, EditText IPM, EditText Cut) {
+        return String.format("%.4f", (Double.valueOf(IPM.getText().toString()) / Double.valueOf(Cut.getText().toString())) / Double.valueOf(RPM.getText().toString()));
+    }*/
+
+    /*private String calculateCut_w_IPM_IPR_RPM(EditText IPM, EditText IPR, EditText RPM) {
+        return String.format("%.4f", (Double.valueOf(IPM.getText().toString()) / Double.valueOf(IPR.getText().toString())) / Double.valueOf(RPM.getText().toString()));
+    }*/
+
+    protected void diaLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM) {
+
+        if (!Dia.getText().toString().equals("") && !SFM.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_Dia_SFM(Dia, SFM));
+
+            if (!IPR.getText().toString().equals("")) {
+                IPM.setText(calculateIPM_w_RPM_IPR(RPM, IPR));
+
+            } else if (!IPM.getText().toString().equals("")) {
+                IPR.setText(calculateIPR_w_RPM_IPM(RPM, IPM));
+
+            }
+        } else if (!Dia.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+        }
 
     }
+
+    /*protected void diaLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM, EditText Cut) {
+        if (!Dia.getText().toString().equals("") && !SFM.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_Dia_SFM(Dia, SFM));
+
+            if (!IPR.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+                IPM.setText(calculateIPM_w_RPM_IPR_Cut(RPM, IPR, Cut));
+
+            } else if (!IPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+                IPR.setText(calculateIPR_w_RPM_IPM_Cut(RPM, IPM, Cut));
+
+            } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("")) {
+                Cut.setText(calculateCut_w_IPM_IPR_RPM(IPM, IPR, RPM));
+
+            }
+        } else if (!Dia.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+        }
+    }*/
+
+    protected void SFMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM) {
+        if (!Dia.getText().toString().equals("") && !SFM.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_Dia_SFM(Dia, SFM));
+
+            if (!IPR.getText().toString().equals("")) {
+                IPM.setText(calculateIPM_w_RPM_IPR(RPM, IPR));
+
+            } else if (!IPM.getText().toString().equals("")) {
+                IPR.setText(calculateIPR_w_RPM_IPM(RPM, IPM));
+
+            }
+        } else if (!SFM.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+        }
+    }
+
+    /*protected void SFMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM, EditText Cut){
+        if (!Dia.getText().toString().equals("") && !SFM.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_Dia_SFM(Dia, SFM));
+
+            if (!IPR.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+                IPM.setText(calculateIPM_w_RPM_IPR_Cut(RPM, IPR, Cut));
+
+            } else if (!IPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+                IPR.setText(calculateIPR_w_RPM_IPM_Cut(RPM, IPM, Cut));
+
+            } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("")) {
+                Cut.setText(calculateCut_w_IPM_IPR_RPM(IPM, IPR, RPM));
+
+            }
+        } else if (!SFM.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+        }
+    }*/
+
+    protected void RPMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM) {
+        if (!IPR.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            IPM.setText(calculateIPM_w_RPM_IPR(RPM, IPR));
+
+        } else if (!IPM.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            IPR.setText(calculateIPR_w_RPM_IPM(RPM, IPM));
+
+        }
+
+        if (!Dia.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+        } else if (!SFM.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+        }
+    }
+
+    /*protected void RPMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM, EditText Cut) {
+        if (!IPR.getText().toString().equals("") && !RPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            IPM.setText(calculateIPM_w_RPM_IPR_Cut(RPM, IPR, Cut));
+
+        } else if (!IPM.getText().toString().equals("") && !RPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            IPR.setText(calculateIPR_w_RPM_IPM_Cut(RPM, IPM, Cut));
+
+        } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Cut.setText(calculateCut_w_IPM_IPR_RPM(IPM, IPR, RPM));
+
+        }
+
+        if (!Dia.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+        } else if (!SFM.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+        }
+    }*/
+
+    protected void IPRLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM) {
+        if (!RPM.getText().toString().equals("") && !IPR.getText().toString().equals("")) {
+            IPM.setText(calculateIPM_w_RPM_IPR(RPM, IPR));
+
+        } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_IPR_IPM(IPR, IPM));
+
+            if (!Dia.getText().toString().equals("")) {
+                SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+            } else if (!SFM.getText().toString().equals("")) {
+                Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+            }
+        }
+    }
+
+    /*protected void IPRLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM, EditText Cut) {
+        if (!RPM.getText().toString().equals("") && !IPR.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            IPM.setText(calculateIPM_w_RPM_IPR_Cut(RPM, IPR, Cut));
+
+        } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_IPR_IPM_Cut(IPR, IPM, Cut));
+
+            if (!Dia.getText().toString().equals("")) {
+                SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+            } else if (!SFM.getText().toString().equals("")) {
+                Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+            }
+        } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Cut.setText(calculateCut_w_IPM_IPR_RPM(IPM, IPR, RPM));
+
+        }
+    }*/
+
+    protected void IPMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM) {
+        if (!RPM.getText().toString().equals("") && !IPM.getText().toString().equals("")) {
+            IPR.setText(calculateIPR_w_RPM_IPM(RPM, IPM));
+
+        } else if (!IPR.getText().toString().equals("") && !IPM.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_IPR_IPM(IPR, IPM));
+
+            if (!Dia.getText().toString().equals("")) {
+                SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+            } else if (!SFM.getText().toString().equals("")) {
+                Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+            }
+        }
+    }
+
+    /*protected void IPMLostFocus(EditText Dia, EditText SFM, EditText RPM, EditText IPR, EditText IPM, EditText Cut) {
+        if (!RPM.getText().toString().equals("") && !IPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            IPR.setText(calculateIPR_w_RPM_IPM_Cut(RPM, IPM, Cut));
+
+        } else if (!IPR.getText().toString().equals("") && !IPM.getText().toString().equals("") && !Cut.getText().toString().equals("")) {
+            RPM.setText(calculateRPM_w_IPR_IPM_Cut(IPR, IPM, Cut));
+
+            if (!Dia.getText().toString().equals("")) {
+                SFM.setText(calculateSFM_w_Dia_RPM(Dia, RPM));
+
+            } else if (!SFM.getText().toString().equals("")) {
+                Dia.setText(calculateDia_w_SFM_RPM(SFM, RPM));
+
+            }
+        } else if (!IPM.getText().toString().equals("") && !IPR.getText().toString().equals("") && !RPM.getText().toString().equals("")) {
+            Cut.setText(calculateCut_w_IPM_IPR_RPM(IPM, IPR, RPM));
+
+        }
+    }*/
 }
--- app/src/main/java/com/howeloh/www/cncbuddy/Drilling.java
+++ app/src/main/java/com/howeloh/www/cncbuddy/Drilling.java
@@ -3,147 +3,84 @@ package com.howeloh.www.cncbuddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class Drilling extends AppCompatActivity {
+public class Drilling extends AppCompatActivity implements View.OnFocusChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drilling);
 
-
-        setFocusChangeListeners();
-    }
-
-    private void setFocusChangeListeners (){
-
         final EditText drillDia = (EditText) findViewById(R.id.drillDiameter);
         final EditText drillSFM = (EditText) findViewById(R.id.drillSurfaceFootage);
         final EditText drillRPM = (EditText) findViewById(R.id.drillRotationalSpeed);
         final EditText drillIPR = (EditText) findViewById(R.id.drillPerRev);
         final EditText drillIPM = (EditText) findViewById(R.id.drillFeed);
-        Calculator calc = new Calculator();
+        final Button clearButton = (Button) findViewById(R.id.clearButton);
 
-        drillDia.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        drillDia.setOnFocusChangeListener(this);
+        drillSFM.setOnFocusChangeListener(this);
+        drillRPM.setOnFocusChangeListener(this);
+        drillIPR.setOnFocusChangeListener(this);
+        drillIPM.setOnFocusChangeListener(this);
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    String DiaString = drillDia.getText().toString();
-                    String SFMString = drillSFM.getText().toString();
-                    String RPMString = drillRPM.getText().toString();
-                    String IPRString = drillIPR.getText().toString();
-                    String IPMString = drillIPM.getText().toString();
-
-                    if (!DiaString.equals("") && !SFMString.equals("")) {
-                        RPMString = calculateRPM_w_Dia_SFM(Double.valueOf(DiaString), Double.valueOf(SFMString));
-                        drillRPM.setText(RPMString);
-
-                        if (!IPRString.equals("")) {
-                            IPMString = calculateIPM_w_RPM_IPR(Double.valueOf(RPMString), Double.valueOf(IPRString));
-                            drillIPM.setText(IPMString);
-
-                        } else if (!IPMString.equals("")){
-                            IPRString = calculateIPR_w_RPM_IPM(Double.valueOf(RPMString), Double.valueOf(IPMString));
-                            drillIPR.setText(IPRString);
-                        }
-
-                    } else if (!DiaString.equals("") && !RPMString.equals("")) {
-                        SFMString = calculateSFM_w_Dia_RPM(Double.valueOf(DiaString), Double.valueOf(RPMString));
-                        drillSFM.setText(SFMString);
-                    }
-                }
+            public void onClick(View v) {
+                drillDia.setText("");
+                drillSFM.setText("");
+                drillRPM.setText("");
+                drillRPM.setText("");
+                drillIPR.setText("");
+                drillIPM.setText("");
             }
         });
 
-        drillSFM.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    String DiaString = drillDia.getText().toString();
-                    String SFMString = drillSFM.getText().toString();
-                    String RPMString = drillRPM.getText().toString();
-                    String IPRString = drillIPR.getText().toString();
-                    String IPMString = drillIPM.getText().toString();
-
-                    if (!DiaString.equals("") && !SFMString.equals("")) {
-                        RPMString = calculateRPM_w_Dia_SFM(Double.valueOf(DiaString), Double.valueOf(SFMString));
-                        drillRPM.setText(RPMString);
-
-                        if (!IPRString.equals("")) {
-                            IPMString = calculateIPM_w_RPM_IPR(Double.valueOf(RPMString), Double.valueOf(IPRString));
-                            drillIPM.setText(IPMString);
-
-                        } else if (!IPMString.equals("")){
-                            IPRString = calculateIPR_w_RPM_IPM(Double.valueOf(RPMString), Double.valueOf(IPMString));
-                            drillIPR.setText(IPRString);
-                        }
-
-                    } else if (!SFMString.equals("") && !RPMString.equals("")) {
-                        DiaString = calculateDia_w_SFM_RPM(Double.valueOf(SFMString), Double.valueOf(RPMString));
-                        drillDia.setText(DiaString);
-
-                    }
-                }
-            }
-        });
-
-        drillRPM.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    String DiaString = drillDia.getText().toString();
-                    String SFMString = drillSFM.getText().toString();
-                    String RPMString = drillRPM.getText().toString();
-                    String IPRString = drillIPR.getText().toString();
-                    String IPMString = drillIPM.getText().toString();
-
-                    if (!IPRString.equals("") && !RPMString.equals("")){
-                        IPMString = calculateIPM_w_RPM_IPR(Double.valueOf(RPMString), Double.valueOf(IPRString));
-                        drillIPM.setText(IPMString);
-
-                    } else if (!IPMString.equals("") && !RPMString.equals("")) {
-                        IPRString = calculateIPM_w_RPM_IPR(Double.valueOf(RPMString), Double.valueOf(IPRString));
-                        drillIPM.setText(IPRString);
-
-                    }
-
-                    if (!DiaString.equals("") && !RPMString.equals("")){
-                        SFMString = calculateSFM_w_Dia_RPM(Double.valueOf(DiaString), Double.valueOf(RPMString));
-                        drillSFM.setText(SFMString);
-
-                    } else if(!SFMString.equals("") && !RPMString.equals("")){
-                        DiaString = calculateDia_w_SFM_RPM(Double.valueOf(SFMString), Double.valueOf(RPMString));
-                        drillDia.setText(DiaString);
-
-                    }
-                }
-            }
-        });
     }
 
-    private String calculateDia_w_SFM_RPM(double SFM, double RPM){
-        return String.format("%.4f", SFM / (RPM * (Math.PI / 12d)));
-    }
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
 
-    private String calculateSFM_w_Dia_RPM (double Dia, double RPM){
-        return String.format("%.4f", Dia * (RPM * (Math.PI / 12d)));
-    }
+        Calculator calculator = new Calculator();
 
-    private String calculateRPM_w_Dia_SFM(double Dia, double SFM){
-        return String.format("%.4f", SFM /(Dia * (Math.PI / 12d)));
-    }
+        if (v.getId() == R.id.drillDiameter && !hasFocus) {
+            calculator.diaLostFocus((EditText) findViewById(R.id.drillDiameter),
+                    (EditText) findViewById(R.id.drillSurfaceFootage),
+                    (EditText) findViewById(R.id.drillRotationalSpeed),
+                    (EditText) findViewById(R.id.drillPerRev),
+                    (EditText) findViewById(R.id.drillFeed));
 
-    private String calculateRPM_w_IPR_IPM (double IPR, double IPM){
-        return String.format("%.4f", IPM/IPR);
-    }
+        } else if (v.getId() == R.id.drillSurfaceFootage && !hasFocus) {
+            calculator.SFMLostFocus((EditText) findViewById(R.id.drillDiameter),
+                    (EditText) findViewById(R.id.drillSurfaceFootage),
+                    (EditText) findViewById(R.id.drillRotationalSpeed),
+                    (EditText) findViewById(R.id.drillPerRev),
+                    (EditText) findViewById(R.id.drillFeed));
 
-    private String calculateIPM_w_RPM_IPR (double RPM, double IPR){
-        return String.format("%.4f", RPM * IPR);
-    }
+        } else if (v.getId() == R.id.drillRotationalSpeed && !hasFocus) {
+            calculator.RPMLostFocus((EditText) findViewById(R.id.drillDiameter),
+                    (EditText) findViewById(R.id.drillSurfaceFootage),
+                    (EditText) findViewById(R.id.drillRotationalSpeed),
+                    (EditText) findViewById(R.id.drillPerRev),
+                    (EditText) findViewById(R.id.drillFeed));
 
-    private String calculateIPR_w_RPM_IPM (double RPM, double IPM){
-        return String.format("%.4f", IPM / RPM);
+        } else if (v.getId() == R.id.drillPerRev && !hasFocus) {
+            calculator.IPRLostFocus((EditText) findViewById(R.id.drillDiameter),
+                    (EditText) findViewById(R.id.drillSurfaceFootage),
+                    (EditText) findViewById(R.id.drillRotationalSpeed),
+                    (EditText) findViewById(R.id.drillPerRev),
+                    (EditText) findViewById(R.id.drillFeed));
+
+        } else if (v.getId() == R.id.drillFeed && !hasFocus) {
+            calculator.IPMLostFocus((EditText) findViewById(R.id.drillDiameter),
+                    (EditText) findViewById(R.id.drillSurfaceFootage),
+                    (EditText) findViewById(R.id.drillRotationalSpeed),
+                    (EditText) findViewById(R.id.drillPerRev),
+                    (EditText) findViewById(R.id.drillFeed));
+
+        }
     }
 }
 
